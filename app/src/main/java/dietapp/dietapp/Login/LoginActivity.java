ac8041dfb5c1eb1e4cef43c01e019e69283caa26
Alex <alexkolydas@gmail.com>
2018-12-05 22:54:55 +0100

    login_xml

---
+++ app/src/main/java/dietapp/dietapp/Login/LoginActivity.java
@@ -0,0 +1,17 @@
+package dietapp.dietapp.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dietapp.dietapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
